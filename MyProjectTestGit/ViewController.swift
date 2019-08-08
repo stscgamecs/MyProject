b03ab8be2780dc7c79e62a44ca26e00bcc9f2365
ZGAME <58160323@go.buu.ac.th>
2019-08-08 14:24:59 +0700

    Initial Commit

---
+++ MyProjectTestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProjectTestGit
+//
+//  Created by Z64me on 8/8/2562 BE.
+//  Copyright © 2562 Z64me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
