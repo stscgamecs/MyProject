506499fca520a4411cd6fcddde610c6b7fa45e7e
ZGAME <58160323@go.buu.ac.th>
2019-09-01 19:20:37 +0700

    Create alert for delete cell

--- MyProjectTestGit/ViewController.swift
+++ MyProjectTestGit/ViewController.swift
@@ -45,11 +45,7 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController:UITableViewDataSource,UITableViewDelegate{
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+extension ViewController:UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print(arrayUser.count)
@@ -63,24 +59,33 @@ extension ViewController:UITableViewDataSource,UITableViewDelegate{
         cell?.setupUI(Joke: data)
         return cell!
     }
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete{
-            arrayUser.remove(at: indexPath.row)
-            tabelViewShow.deleteRows(at:[indexPath], with: .fade)
-        }
-    }
+}
+
+extension ViewController:UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Main",bundle: nil)
         
         guard let detailViewController = storyboard.instantiateViewController(withIdentifier: "tableViewDetail") as?  DetailNewViewController else{
             return
         }
-
         let item1 = arrayUser[indexPath.item].appeared_at
         let item2 = arrayUser[indexPath.item].tags[0]
         
         detailViewController.setTitle(title: item1, title2: item2)
-       self.navigationController?.pushViewController(detailViewController, animated: true)
+        self.navigationController?.pushViewController(detailViewController, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            
+            let alert = UIAlertController(title: "You want to delete this cell", message: "Delete", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler:{action in
+                self.arrayUser.remove(at: indexPath.row)
+                self.tabelViewShow.deleteRows(at:[indexPath], with: .fade)
+            }))
+            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+            
+            self.present(alert, animated: true)
+        }
     }
 }
-
